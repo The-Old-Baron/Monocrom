8e794011fe79ecc54d845563455989425ee0cf83
Joao Vitor Ferreira <42494721+MestreSol@users.noreply.github.com>
2024-08-31 11:46:50 -0300

    Update Entity.cs

--- Assets/Scripts/Model/Entity.cs
+++ Assets/Scripts/Model/Entity.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class Entity : MonoBehaviour
 {
+    private float _health;
+    private float _maxHealth;
+    private float _stamina;
+    private float _maxStamina;
+    private float _mana;
+    private float _maxMana;
+
     public string Name;
-    public float Health;
-    public float MaxHealth;
+    public float Health { 
+        get { return _health; } 
+        set { _health = value; } 
+    }
+    public float MaxHealth { 
+        get { return _maxHealth; } 
+        set { _maxHealth = value; } 
+    }
+    public float Stamina { 
+        get { return _stamina; } 
+        set { _stamina = value; }
+    }
+    public float MaxStamina {
+        get { return _maxStamina; }
+        set { _maxStamina = value; }
+    }
+    public float Mana { 
+        get { return _mana; } 
+        set { _mana = value; } 
+    }
+    public float MaxMana
+    {
+        get { return _maxMana; }
+        set { _maxMana = value; }
+    }
 
     public int Level;
     public int Experience;

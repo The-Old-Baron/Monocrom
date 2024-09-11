f28d90f5b4a89407c5a51fdc2c9e868a7208bd2e
Alvaro Claro <alvarocda@gmail.com>
2024-09-10 20:46:08 -0400

    Adicionado animações

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class PlayerController : Entity
 {
     public Rigidbody2D rg;
-    public Animation anim;
+    public Animator animator;
     public BoxCollider2D playerCollider;
     
 
@@ -34,13 +34,14 @@ public class PlayerController : Entity
     public bool isDoubleJumping;
     public bool isChrouching;
 
-    private float lastDashTime = -10f;
+    
     // Variáveis para detectar duplo toque
-    private float lastTapTimeD;
-    private float lastTapTimeA;
+    private float _lastTapTimeD;
+    private float _lastTapTimeA;
 
     public float dashTime;
     public float dashCooldownTime;
+    
     
     
 
@@ -53,44 +54,46 @@ public class PlayerController : Entity
     public float groundCheckRadius;
     public float wallCheckDistance;
     
-    private Vector2 crouchSize;
-    private Vector2 crouchOffset;
+    private Vector2 _crouchSize;
+    private Vector2 _crouchOffset;
     
-    private Vector2 originalSize;
-    private Vector2 originalOffset;
+    private Vector2 _originalSize;
+    private Vector2 _originalOffset;
     
     
     // Essas 2 variaveis abaixo é só pra ver o cubo "agachando", quando for o sprite mesmo a gente usa o sprite agachando
-    private Vector3 originalScale;
-    private Vector3 crouchScale;
-    private Vector2 movement;
+    private Vector3 _originalScale;
+    private Vector3 _crouchScale;
+    private Vector2 _movement;
+    private SpriteRenderer _spriteRenderer;
 
     public DirectionMove directionMovement;
     private void Start()
     {
         rg = GetComponent<Rigidbody2D>();
         directionMovement = DirectionMove.Right;
-        // anim = GetComponent<Animation>();
+        
         playerCollider = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+        _originalOffset = playerCollider.offset;
+        _originalSize = playerCollider.size;
 
-        originalOffset = playerCollider.offset;
-        originalSize = playerCollider.size;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
         
-        
-        crouchSize = new Vector2(originalSize.x, originalSize.y / 2);
-        crouchOffset = new Vector2(originalOffset.x, originalSize.y / 2);
+        _crouchSize = new Vector2(_originalSize.x, _originalSize.y / 2);
+        _crouchOffset = new Vector2(_originalOffset.x, _originalSize.y / 2);
 
 
-        originalScale = transform.localScale;
-        crouchScale = new Vector3(originalScale.x, originalScale.y / 2, originalScale.z);
+        _originalScale = transform.localScale;
+        _crouchScale = new Vector3(_originalScale.x, _originalScale.y / 2, _originalScale.z);
     }
 
     private void Update()
     {
         
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);    
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
         isWalled = Physics2D.Raycast(wallCheck.position, transform.right, wallCheckDistance, wallLayer);
         if (isGrounded)
         {
@@ -134,29 +137,34 @@ public class PlayerController : Entity
         {
             isRunning = false;
         }
+
+        if (_movement.x == 0)
+        {
+            animator.Play("Idle");
+        }
     }
 
     private void DetectDash()
     {
         if (Input.GetKeyDown(KeyCode.D))
         {
-            if (Time.time - lastTapTimeD < doubleTapTimeWindow && !isDashing && Time.time >= dashCooldownTime)
+            if (Time.time - _lastTapTimeD < doubleTapTimeWindow && !isDashing && Time.time >= dashCooldownTime)
             {
                 Debug.Log("Dashing Right");
                 Dash(Vector2.right);
             }
-            lastTapTimeD = Time.time;
+            _lastTapTimeD = Time.time;
         }
 
         // Detecta duplo toque na tecla "A"
         if (Input.GetKeyDown(KeyCode.A))
         {
-            if (Time.time - lastTapTimeA < doubleTapTimeWindow && !isDashing && Time.time >= dashCooldownTime)
+            if (Time.time - _lastTapTimeA < doubleTapTimeWindow && !isDashing && Time.time >= dashCooldownTime)
             {
                 Debug.Log("Dashing Left");
                 Dash(Vector2.left);
             }
-            lastTapTimeA = Time.time;
+            _lastTapTimeA = Time.time;
         }
 
         if (isDashing)
@@ -175,7 +183,7 @@ public class PlayerController : Entity
         // O size reduz o tamanho do colisor e o offset acredito que não vai precisar
         // mas de todo modo vou deixar ele comentado por que pode ser que ajude
         isChrouching = true;
-        transform.localScale = crouchScale;
+        transform.localScale = _crouchScale;
         // playerCollider.size = crouchScale;
         // playerCollider.offset = new Vector2(0,-0.25f);
     }
@@ -183,9 +191,9 @@ public class PlayerController : Entity
     private void Standup()
     {
         isChrouching = false;
-        transform.localScale = originalScale;
-        playerCollider.size = originalSize;
-        playerCollider.offset = originalOffset;
+        transform.localScale = _originalScale;
+        playerCollider.size = _originalSize;
+        playerCollider.offset = _originalOffset;
     }
 
     private IEnumerator JumpDown()
@@ -205,11 +213,14 @@ public class PlayerController : Entity
     }
 
     private void FixedUpdate()
-    {
-        if (movement.x > 0) {
+    { 
+        if (_movement.x > 0) {
             directionMovement = DirectionMove.Right;
-        } else if (movement.x < 0) {
+            _spriteRenderer.flipX = false;
+
+        } else if (_movement.x < 0) {
             directionMovement = DirectionMove.Left;
+            _spriteRenderer.flipX = true;
         }
 
         if (isGrounded && !isJumping)
@@ -271,9 +282,9 @@ public class PlayerController : Entity
         rg.velocity = Vector2.zero;
 
         // Faz com que o jogador continue caminhando caso o player esteja apertando algum botão de movimento
-        if (movement.x != 0)
+        if (_movement.x != 0)
         {
-            rg.velocity = new Vector2(movement.x * walkSpeed, rg.velocity.y);
+            rg.velocity = new Vector2(_movement.x * walkSpeed, rg.velocity.y);
         }
     }
 
@@ -282,7 +293,7 @@ public class PlayerController : Entity
         float move = Input.GetAxis("Horizontal");
         rg.velocity = new Vector2(move * runSpeed, rg.velocity.y);
 
-        // anim.Play("Run");
+        animator.Play("Run");
     }
 
     private void Jump()
